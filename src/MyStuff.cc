65b96b069c277a043fdc5dd6aa290b127dc52905
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-13 03:35:18 +0530

    Create MyStuff.cc

---
+++ src/MyStuff.cc
@@ -0,0 +1,65 @@
+// -*- C++ -*-
+//
+// Package:     MyStuff/src
+// Class  :     MyStuff
+// 
+// Implementation:
+//     [Notes on implementation]
+//
+// Original Author:  Shravan Sunil Chaudhary
+//         Created:  Sun, 12 Jul 2020 22:04:27 GMT
+//
+
+// system include files
+
+// user include files
+#include "MyStuff.h"
+
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+MyStuff::MyStuff()
+{
+}
+
+// MyStuff::MyStuff(const MyStuff& rhs)
+// {
+//    // do actual copying here;
+// }
+
+MyStuff::~MyStuff()
+{
+}
+
+//
+// assignment operators
+//
+// const MyStuff& MyStuff::operator=(const MyStuff& rhs)
+// {
+//   //An exception safe implementation is
+//   MyStuff temp(rhs);
+//   swap(rhs);
+//
+//   return *this;
+// }
+
+//
+// member functions
+//
+
+//
+// const member functions
+//
+
+//
+// static member functions
+//
