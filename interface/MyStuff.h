75bdcaa3b6a2c912bea00cedd83e7b0fc2cfc035
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-13 03:32:55 +0530

    Create MyStuff.h

---
+++ interface/MyStuff.h
@@ -0,0 +1,50 @@
+#ifndef MyStuff_interface_MyStuff_h
+#define MyStuff_interface_MyStuff_h
+// -*- C++ -*-
+//
+// Package:     MyStuff/interface
+// Class  :     MyStuff
+// 
+/**\class MyStuff MyStuff.h "MyStuff.h"
+
+ Description: [one line class summary]
+
+ Usage:
+    <usage>
+
+*/
+//
+// Original Author:  Shravan Sunil Chaudhary
+//         Created:  Sun, 12 Jul 2020 21:59:03 GMT
+//
+
+// system include files
+
+// user include files
+
+// forward declarations
+
+class MyStuff
+{
+
+   public:
+      MyStuff();
+      virtual ~MyStuff();
+
+      // ---------- const member functions ---------------------
+
+      // ---------- static member functions --------------------
+
+      // ---------- member functions ---------------------------
+
+   private:
+      MyStuff(const MyStuff&); // stop default
+
+      const MyStuff& operator=(const MyStuff&); // stop default
+
+      // ---------- member data --------------------------------
+
+};
+
+
+#endif
