3be953b18b8bff45b66228ef303202acd7ab1faa
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-13 03:36:03 +0530

    Create classes.h

---
+++ src/classes.h
@@ -0,0 +1,10 @@
+#include "Analysis/MyStuff/interface/MyStuff.h"
+#include "Analysis/MyStuff/interface/MyOtherStuff.h"
+#include <vector>
+#include "DataFormats/Common/interface/Wrapper.h"
+
+namespace { namespace {
+  //say which template classes should have dictionaries
+  edm::Wrapper<MyStuff> dummy1;
+  edm::Wrapper<std::vector<MyOtherStuff> > dummy2;
+} }
